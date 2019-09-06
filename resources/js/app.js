40b43bd7c62c018020dcdc62dc028d310bba0be8
oooabdooo2010 <aaar_2006@yahoo.com>
2019-09-06 23:17:55 +0200

    Purpose project

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,14 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require('./bootstrap')
 
-window.Vue = require('vue');
+window.Vue = require('vue')
+
+import VueRouter from "vue-router"
+
+Vue.use(VueRouter)
+
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,14 +24,21 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+let routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/profile', component: require('./components/Profile.vue') }
+]
+const router = new VueRouter({
+    routes // short for `routes: routes`
+})
 
 const app = new Vue({
-    el: '#app',
-});
+    router
+}).$mount('#app')
